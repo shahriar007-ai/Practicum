1d2cc022ca1fc25ed92f81228e69f3ae6ef173e5
shahriar007-ai <shahriar888888@gmail.com>
2021-12-08 01:07:10 +0600

    Template

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PostController;
+use App\Http\Controllers\HomeController;
 
 /*
 |--------------------------------------------------------------------------
@@ -16,3 +18,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+
+Route::get('/home', [HomeController::class, 'index']);
