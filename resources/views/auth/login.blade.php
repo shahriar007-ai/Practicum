dbaf419df4ca74853903d6f488e7b223f4b1c977
shahriar007-ai <shahriar888888@gmail.com>
2021-12-13 01:12:30 +0600

    user login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,73 +1,92 @@
-@extends('layouts.app')
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="description" content="">
+    <meta name="author" content="">
 
-@section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">{{ __('Login') }}</div>
+    <title>eShop Admin - Login</title>
 
-                <div class="card-body">
-                    <form method="POST" action="{{ route('login') }}">
-                        @csrf
+    <!-- Custom fonts for this template-->
+    <link href="{{asset('admin_assets/vendor/fontawesome-free/css/all.min.css')}}" rel="stylesheet" type="text/css">
+    <link
+        href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
+        rel="stylesheet">
 
-                        <div class="row mb-3">
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
+    <!-- Custom styles for this template-->
+    <link href="{{asset('admin_assets/css/sb-admin-2.min.css')}}" rel="stylesheet">
+    <style>
+        .bg-login-background{
+            background:url(/admin_assets/img/book-covers.jpg);
+            background-size:cover;
+            background-position:center:
+        }
+    </style>
 
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
+</head>
 
-                                @error('email')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
+<body class="bg-gradient-light">
 
-                        <div class="row mb-3">
-                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
+    <div class="container">
 
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
+        <!-- Outer Row -->
+        <div class="row justify-content-center">
 
-                                @error('password')
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $message }}</strong>
-                                    </span>
-                                @enderror
-                            </div>
-                        </div>
+            <div class="col-xl-10 col-lg-12 col-md-9">
 
-                        <div class="row mb-3">
-                            <div class="col-md-6 offset-md-4">
-                                <div class="form-check">
-                                    <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
-
-                                    <label class="form-check-label" for="remember">
-                                        {{ __('Remember Me') }}
-                                    </label>
+                <div class="card o-hidden border-0 shadow-lg my-5">
+                    <div class="card-body p-0">
+                        <!-- Nested Row within Card Body -->
+                        <div class="row">
+                            <div class="col-lg-6 d-none d-lg-block bg-login-background"></div>
+                            <div class="col-lg-6 bg-danger">
+                                <div class="p-5">
+                                    <div class="text-center">
+                                        <h1 class="h4  text-light mb-4">Login</h1>
+                                        <img width="150" height="150" class="rounded mb-4" src="{{asset('admin_assets/img/undraw_profile_2.svg')}}" alt="">
+                                        @if(Session::has('error'))
+                                            <div class="alert alert-danger">
+                                                {{Session::get('error')}}
+                                            </div>
+                                        @endif
+                                    </div>
+                                    <form class="user" method="POST" action="{{ route('login') }}">
+                                        @csrf
+                                        <div class="form-group mb-4">
+                                            <input type="email" value="{{old('email')}}" name="email" class="form-control @error('email') is-invalid @enderror form-control-user"id="exampleInputEmail" aria-describedby="emailHelp" placeholder="Enter Email Address...">
+                                            @error('email')
+                                                <span class="invalid-feedback" role="alert">
+                                                    <strong>{{ $message }}</strong>
+                                                </span>
+                                            @enderror
+                                        </div>
+                                        <div class="form-group mb-4">
+                                            <input type="password" name="password" class="form-control @error('email') is-invalid @enderror form-control-user" id="exampleInputPassword" placeholder="Password">
+                                            @error('password')
+                                                <span class="invalid-feedback" role="alert">
+                                                    <strong>{{ $message }}</strong>
+                                                </span>
+                                            @enderror
+                                        </div>
+                                        <button type="submit" class="btn btn-primary btn-user btn-block">Login</button>
+                                    </form>
                                 </div>
                             </div>
                         </div>
-
-                        <div class="row mb-0">
-                            <div class="col-md-8 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
-                                    {{ __('Login') }}
-                                </button>
-
-                                @if (Route::has('password.request'))
-                                    <a class="btn btn-link" href="{{ route('password.request') }}">
-                                        {{ __('Forgot Your Password?') }}
-                                    </a>
-                                @endif
-                            </div>
-                        </div>
-                    </form>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
-</div>
-@endsection
+    <script src="{{asset('admin_assets/adminvendor/jquery/jquery.min.js')}}"></script>
+    <script src="{{asset('admin_assets/vendor/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
+    <!-- Core plugin JavaScript-->
+    <script src="{{asset('admin_assets/vendor/jquery-easing/jquery.easing.min.js')}}"></script>
+    <!-- Custom scripts for all pages-->
+    <script src="{{asset('admin_assets/js/sb-admin-2.min.js')}}"></script>
+
+</body>
+
+</html>
---
+++ resources/views/user/dashboard.blade.php
@@ -0,0 +1,13 @@
+@extends('frontend.layouts.master')
+@section('content')
+
+<div class="cart-main-area section-padding--lg bg--white">
+	<div class="container">
+		<div class="row">
+			<div class="col-md-12 col-sm-12 ol-lg-12">
+				<p class="bg-dark text-light p-2 text-center">welcome to dashboard</p>
+			</div>
+		</div>
+	</div>  
+</div>		
+@endsection
