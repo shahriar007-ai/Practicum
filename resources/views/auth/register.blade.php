c90eb4e6044537c6ea622c5afbdc81ef29f304ed
shahriar007-ai <shahriar888888@gmail.com>
2021-12-15 22:08:20 +0600

    register

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -23,6 +23,10 @@
             background-size:cover;
             background-position:center:
         }
+        .card-width{
+            width:600px;
+        }
+
     </style>
 
 </head>
@@ -30,7 +34,7 @@
 <body class="bg-gradient-light">
 
     <div class="container">
-        <div class="card o-hidden border-0 shadow-lg my-5">
+        <div class="card o-hidden border-0 shadow-lg my-5 card-width">
             <div class="card-body p-0">
                 <!-- Nested Row within Card Body -->
                 <div class="row">
